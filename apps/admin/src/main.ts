d4b3c7cbcae11161c7e41dbc65f7883d53a304e5
AndreyA <andreev.avx@gmail.com>
2026-08-17 22:48:29 +0300

    feat: bootstrap AG Grid Enterprise in Admin

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,1 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser'; import { AppComponent } from './app/app.component'; import { appConfig } from './app/app.config'; bootstrapApplication(AppComponent,appConfig).catch(console.error);
+import { bootstrapApplication } from '@angular/platform-browser';
+import { configureAgGridEnterprise } from '@proto/ui-ag-grid';
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+
+configureAgGridEnterprise();
+
+bootstrapApplication(AppComponent, appConfig).catch(console.error);
