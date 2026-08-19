e0898bff9ba1d6afb62f400861618ce5d37fc8c8
AndreyA <andreev.avx@gmail.com>
2026-08-20 00:02:33 +0300

    refactor(ui): remove old Proto UI index

--- libs/ui/proto/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/proto-badge.component';
-export * from './lib/proto-button.directive';
-export * from './lib/proto-control.directive';
-export * from './lib/proto-dialog.component';
-export * from './lib/proto-field.component';
