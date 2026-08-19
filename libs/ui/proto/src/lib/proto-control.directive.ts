ba7e54acc372a3d791617fecd87f38da770a2ca3
AndreyA <andreev.avx@gmail.com>
2026-08-20 00:02:56 +0300

    refactor(ui): remove old Proto control directive

--- libs/ui/proto/src/lib/proto-control.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive } from '@angular/core';
-
-@Directive({
-  selector: 'input[protoInput], textarea[protoInput], select[protoSelect]',
-  standalone: true,
-  host: { class: 'proto-control' },
-})
-export class ProtoControlDirective {}
