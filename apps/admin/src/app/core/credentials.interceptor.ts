74c969505237cc2c575502e21d41c16bc3333f5b
AndreyA <andreev.avx@gmail.com>
2026-08-19 23:26:43 +0300

    refactor: remove cookie credentials interceptor

--- apps/admin/src/app/core/credentials.interceptor.ts
+++
@@ -1,7 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-export const credentialsInterceptor: HttpInterceptorFn = (req, next) =>
-  next(
-    req.clone({
-      withCredentials: true,
-    }),
-  );
