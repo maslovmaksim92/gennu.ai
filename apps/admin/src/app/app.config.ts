bc1d02de41d7ffb1656796ff5602ebdb3089fcce
AndreyA <andreev.avx@gmail.com>
2026-08-19 23:26:36 +0300

    refactor: register bearer token interceptor

--- apps/admin/src/app/app.config.ts
+++ apps/admin/src/app/app.config.ts
@@ -3,11 +3,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideTaiga } from '@taiga-ui/core';
 import { routes } from './app.routes';
-import { credentialsInterceptor } from './core/credentials.interceptor';
+import { authTokenInterceptor } from './core/auth-token.interceptor';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([credentialsInterceptor])),
+    provideHttpClient(withInterceptors([authTokenInterceptor])),
     provideTaiga(),
   ],
 };
