9257bf6f9245d25f1b003e78d7a9bba40630194b
AndreyA <andreev.avx@gmail.com>
2026-08-19 23:59:53 +0300

    refactor(ui): rename Proto UI to Atlas UI

---
+++ libs/ui/atlas/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/atlas-badge.component';
+export * from './lib/atlas-button.directive';
+export * from './lib/atlas-control.directive';
+export * from './lib/atlas-dialog.component';
+export * from './lib/atlas-field.component';
