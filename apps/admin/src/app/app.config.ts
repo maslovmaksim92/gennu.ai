353df86e02e03e6ecd86e79cd8265f6c302dac2b
Andrey <Andrey@DESKTOP-7K7KR01>
2026-08-20 20:22:41 +0300

    taiga ui update lanquage

--- apps/admin/src/app/app.config.ts
+++ apps/admin/src/app/app.config.ts
@@ -4,12 +4,12 @@ import { provideRouter } from '@angular/router';
 import { provideTaiga, tuiAssetsPathProvider } from '@taiga-ui/core';
 import { routes } from './app.routes';
 import { authTokenInterceptor } from './core/auth-token.interceptor';
+import { taigaUIProviders } from '@atlas/taiga-ui';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authTokenInterceptor])),
-    tuiAssetsPathProvider('/assets/taiga-ui/icons'),
-    provideTaiga(),
+    provideHttpClient(withInterceptors([authTokenInterceptor])),    
+    taigaUIProviders
   ],
 };
---
+++ libs/ui/taiga-ui/src/lib/taiga-ui-providers.ts
@@ -0,0 +1,36 @@
+import { Provider, signal } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { tuiButtonOptionsProvider, tuiCheckboxOptionsProvider, tuiRadioOptionsProvider, tuiScrollbarOptionsProvider, tuiTextfieldOptionsProvider, tuiValidationErrorsProvider } from '@taiga-ui/core';
+import {
+  tuiSwitchOptionsProvider,
+  tuiTextareaOptionsProvider,
+} from '@taiga-ui/kit';
+import { of } from 'rxjs';
+import { TUI_LANGUAGE, TUI_RUSSIAN_LANGUAGE } from '@taiga-ui/i18n';
+import { provideTaiga, tuiAssetsPathProvider } from '@taiga-ui/core';
+
+export const taigaUIProviders = [
+  provideAnimations(),
+  provideTaiga(),
+  tuiAssetsPathProvider('/assets/taiga-ui/icons'),
+  tuiValidationErrorsProvider({
+    required: $localize`Заполните поле`,
+    email: $localize`Введите действительный адрес электронной почты`,
+    fileAccept: $localize`Недопустимое расширение файла`,
+    filesRequired: $localize`Файлы не приложены`,
+    isActive: $localize`Поле не должно содержать неактивные элементы`,
+    inactiveElement: $localize`Поле не должно содержать неактивные элементы`,
+    invalidEmails: (message) => `Невалидные почтовые ящики: ${message}`,
+    isEmail: () => $localize`Указано некорректное значение`,
+    isDefault: $localize`По умолчанию может быть выбрано только одно значение`,
+    message: (message: string) => message,
+  }),
+  tuiTextfieldOptionsProvider({ size: signal('s') }),
+  tuiTextareaOptionsProvider({ max: 20 }),
+  tuiButtonOptionsProvider({ size: 's' }),
+  tuiCheckboxOptionsProvider({ size: 's' }),
+  tuiRadioOptionsProvider({ size: 's' }),
+  tuiScrollbarOptionsProvider({ mode: 'native' }),
+  tuiSwitchOptionsProvider({ size: 'm' }),
+  { provide: TUI_LANGUAGE, useValue: signal(TUI_RUSSIAN_LANGUAGE) },
+] satisfies Provider[];
