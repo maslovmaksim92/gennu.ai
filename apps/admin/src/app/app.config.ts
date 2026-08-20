3349a711390f134aa9be377da0fd836855bf93b5
AndreyA <andreev.avx@gmail.com>
2026-08-20 19:08:46 +0300

    feat(admin): configure Taiga UI assets path

--- apps/admin/src/app/app.config.ts
+++ apps/admin/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideTaiga } from '@taiga-ui/core';
+import { provideTaiga, tuiAssetsPathProvider } from '@taiga-ui/core';
 import { routes } from './app.routes';
 import { authTokenInterceptor } from './core/auth-token.interceptor';
 
@@ -9,6 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authTokenInterceptor])),
+    tuiAssetsPathProvider('/assets/taiga-ui/icons'),
     provideTaiga(),
   ],
 };
